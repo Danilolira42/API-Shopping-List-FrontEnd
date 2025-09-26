cd9510e3c3193b8b2d7f38e34f262b56bb38c6e1
Danilo Lira <danilolira0719@gmail.com>
2025-09-25 22:09:14 -0300

    Update

--- script.js
+++ script.js
@@ -14,28 +14,42 @@ form.onsubmit = (event) => {
         const footer = document.querySelector("footer");
         const main = document.querySelector("main");
 
+        
         footer.style.display = "flex";
         console.log(main)
 
     } else {
 
-
     const newItem = document.createElement("li");
     const newLabel = document.createElement("label");
     const newInput = document.createElement("input");
     const newIcon = document.createElement("i");
+    
+    const regex = /\D+/g
+    
+    const replace = input.value.match(regex);
 
+    const changes = replace.toString().toLowerCase();
+
+    const firstCharacter = changes.charAt(0);
+
+    const upperCharacter = firstCharacter.toString().toUpperCase();
+    
+    const newString = changes.replace(changes.charAt(0), upperCharacter);
     
     newInput.type = "checkbox";
     newItem.appendChild(newInput);
     
-    newLabel.textContent = input.value;
+    newLabel.textContent = newString;
     newItem.appendChild(newLabel);
 
     newIcon.classList.add("hgi", "hgi-stroke", "hgi-delete-02");
     newItem.appendChild(newIcon);
+    
+
+    console.log(newItem)
 
     list.appendChild(newItem);
-
+    
     input.value = "";
 }};
